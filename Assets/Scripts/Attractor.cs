5e67f70e7b261b2f5b7226b11b1d0761c50b0dab
David K <davek032@outlook.com>
2021-03-03 16:18:02 +0000

    Asteroid spawning & hurtling
    Asteroids are now 'ejected' from the belt with random force and will hurtle in the direction of earth.

---
+++ Assets/Scripts/Attractor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    public Rigidbody rb;
+    const float G = 667.4f;
+
+    public bool isAttractor;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+        foreach (Attractor item in attractors)
+        {
+            if (item != this)
+            {
+                Attract(item);
+            }
+        }
+    }
+
+    void Attract(Attractor objToAttract) 
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+
+    }
+}
