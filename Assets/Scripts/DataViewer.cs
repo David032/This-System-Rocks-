8707cf4c41a7c0f4819cd2c89674e1f700546c43
David K <davek032@outlook.com>
2021-03-14 15:49:46 +0000

    Asteroid Data viewer
    Clicking on an asteroid now displays some info about it and deletes the object

---
+++ Assets/Scripts/DataViewer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class DataViewer : MonoBehaviour
+{
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI CloseApproach;
+    public TextMeshProUGUI RelVel;
+    public TextMeshProUGUI Diameter;
+}
